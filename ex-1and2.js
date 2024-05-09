a6dd98f50de96e27560e6a778fd1fff742f94316
Athikhom_ra <athikhom_ra@outlook.com>
2024-05-09 13:52:01 +0700

    feat: answer assingment ex1-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,16 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let sureName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const totalDiningPrice = 15000;
+const amoutRestaurantOpened = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalDiningPrice = 5000;
+amoutRestaurantOpened = 2500;
