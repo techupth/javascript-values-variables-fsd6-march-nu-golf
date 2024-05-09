ec2a260472fac4a9cdb90dcb4ae3b1c7b5744dd8
Kunakorn <kunakorn.ph98@gmail.com>
2024-05-09 13:55:09 +0700

    Assignment#1 March

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
-//Exercise #1: Variable Creation
-//Start coding here
-
-//Exercise #2: Variable Reassignment
-//Start coding here
+// Exercise #1: Variable Creation 
+// Start coding here 
+let name = 'Karter'
+let surname = 'Anderson'
+let address = '54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai'
+const quot = "Chef's Table by Chef James"
+const bill = 15000
+const totalOpenRestuarant = 1500
+// Exercise #2: Variable Reassignment
+// Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+bill = 5000
+totalOpenRestuarant = 2500
